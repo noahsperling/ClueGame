3bd8cdb210fc70e8fbe7a135425849f217b6a303
noahsperling <sperling19@up.edu>
2016-10-25 19:47:00 -0700

    made a couple classes

---
+++ app/src/main/java/edu/up/cs301/game/ClueGameState.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.game;
+
+import edu.up.cs301.game.infoMsg.GameState;
+
+/**
+ * Created by Noah on 10/25/2016.
+ */
+
+public class ClueGameState extends GameState {
+
+
+
+}
---
+++ app/src/main/java/edu/up/cs301/game/ClueLocalGame.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.game;
+
+/**
+ * Created by Noah on 10/25/2016.
+ */
+
+public class ClueLocalGame extends LocalGame {
+
+    public ClueGameState() {
+
+    }
+
+}
