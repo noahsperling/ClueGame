a97879a0c4b840682452ac2aecfa06f95f885de1
noahsperling <sperling19@up.edu>
2016-11-06 21:17:35 -0800

    made a couple classes

--- app/src/main/java/edu/up/cs301/game/ClueGameState.java
+++ app/src/main/java/edu/up/cs301/game/ClueGameState.java
@@ -8,6 +8,38 @@ import edu.up.cs301.game.infoMsg.GameState;
 
 public class ClueGameState extends GameState {
 
+    private int turnID;
+    private int numPlayers;
+    private int playerIDs[] = new int[numPlayers]; //0 - 5 max
+    private int dieValue;
+    private int spacesMoved;
+    private String notes[];
+    private String playerNames[];
+    private int boardTiles[][] = new int[22][19];
+    private boolean canSuggest[] = new boolean[numPlayers];
+    private boolean canRoll[] = new boolean[numPlayers];
+    //private Card cards[][] = new Card[numPlayers][7];
+
+    private int getTurnId() {
+        return turnID;
+    }
+    private int getDieValue() {
+        return dieValue;
+    }
+    int getSpacesMoved() {
+        return spacesMoved;
+    }
+    int getRoom(int playerID) {
+        //flesh out more
+        return 1;
+    }
+    String getNotes(int playerID) {
+        return notes[playerID];
+    }
+
+
+
+
 
 
 }
