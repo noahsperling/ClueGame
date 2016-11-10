b5a5ec643b5b4f5980d0f777279c999003e80c9b
noahsperling <sperling19@up.edu>
2016-11-09 16:55:52 -0800

    made a quick change

--- app/src/test/java/edu/up/cs301/game/ClueLocalGameTest.java
+++ app/src/test/java/edu/up/cs301/game/ClueLocalGameTest.java
@@ -59,7 +59,7 @@ public class ClueLocalGameTest {
         ClueLocalGame cLG = new ClueLocalGame();
         CluePlayer p1 = new CluePlayer();
         cLG.sendUpdatedStateTo(p1);
-        assertTrue("State has not been updated",p1.getRecentState() == cLG.state);
+        assertTrue("State has not been updated",p1.getRecentState().equals(cLG.state));
 
 
     }
