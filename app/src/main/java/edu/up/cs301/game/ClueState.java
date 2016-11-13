d08080c3489b856fdf233cf3042757a421b12277
noahsperling <sperling19@up.edu>
2016-11-13 14:39:05 -0800

    ClueStateConstructor 2 looks ok needs work

--- app/src/main/java/edu/up/cs301/game/ClueState.java
+++ app/src/main/java/edu/up/cs301/game/ClueState.java
@@ -10,19 +10,34 @@ public class ClueState extends GameState {
 
     private int turnID;
     private int numPlayers;
-    private int playerIDs[] = new int[numPlayers]; //0 - 5 max
+    private int playerIDs[];// = new int[numPlayers]; //0 - 5 max
     private int dieValue;
     private int spacesMoved;
-    private String notes[] = new String[numPlayers];
+    private String notes[];// = new String[numPlayers];
     private String playerNames[];
-    private int boardTiles[][] = new int[22][19];
-    private boolean canSuggest[] = new boolean[numPlayers];
-    private boolean canRoll[] = new boolean[numPlayers];
-    //private Card cards[][] = new Card[numPlayers][7];
-    private boolean gameOver = false;
+    private int boardTiles[][];// = new int[22][19];
+    private boolean canSuggest[];// = new boolean[numPlayers];
+    private boolean canRoll[];// = new boolean[numPlayers];
+    //private Card cards[][];// = new Card[numPlayers][7];
+    private boolean gameOver; // = false;
 
-    public ClueState() {
+    public ClueState(int initNumPlayers, String initPlayerNames[], int initTurnID) {
+        turnID = initTurnID;
+        numPlayers = initNumPlayers;
+        playerIDs = new int[numPlayers];
+        playerNames = new String[numPlayers];
+        canSuggest = new boolean[numPlayers];
+        canRoll = new boolean[numPlayers];
+        //cards[][] = new Card[numPlayers][7];
+        gameOver = false;
 
+        for(int i = 0; i < numPlayers; i++) {
+            playerIDs[i] = i;
+            canSuggest[i] = false;
+            canRoll[i] = false;
+            notes[i] = "";
+            playerNames[i] = new String(initPlayerNames[i]);
+        }
     }
 
     public ClueState(ClueState s) {
