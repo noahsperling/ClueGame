acc0537f233308f17213324b036fd6496ae3b3dc
noahsperling <sperling19@up.edu>
2016-11-13 15:05:52 -0800

    Hand class

---
+++ app/src/main/java/edu/up/cs301/game/Hand.java
@@ -0,0 +1,26 @@
+package edu.up.cs301.game;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Noah on 11/13/2016.
+ */
+
+public class Hand {
+    ArrayList<Card> cards;
+
+    public Hand() {
+        cards = new ArrayList<Card>();
+    }
+
+    public void addCard(Card c) {
+        cards.add(c);
+    }
+
+    public void getCards() {
+        Card tempCardArray[] = new Card[cards.size()];
+        for(int i = 0; i < cards.size(); i++) {
+            tempCardArray[i] = cards.get(i);
+        }
+    }
+}
